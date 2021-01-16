b20a38b785ffab4911637d74f70d6414d033e8a0
Dylan Shorten <Dylan_Shorten@Outlook.com>
2021-01-16 13:32:39 +1100

    Commit ClockDateTime class (Was excluded in last commit)

---
+++ Mighty Kingdom Code Test/Assets/Scripts/ClockDateTime.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+
+
+[Flags]
+public enum EDateTimeFormat
+{
+    Millisecond = 1 << 1,
+    Second = 1 << 2,
+    Minute = 1 << 3,
+    Hour = 1 << 4,
+    Day = 1 << 5,
+    Month = 1 << 6,
+    Year = 1 << 7,
+}
+
+
+[Serializable]
+public class ClockDateTime : ISerializationCallbackReceiver
+{
+    public DateTime DateTime { get => dateTime; set => dateTime = value; }
+
+    public EDateTimeFormat DateTimeFormat => dateTimeFormat;
+
+    DateTime dateTime = default;
+
+    [SerializeField]
+    EDateTimeFormat dateTimeFormat = 0;
+
+    [SerializeField]
+    int millisecond = default;
+
+    [SerializeField]
+    int second = default;
+
+    [SerializeField]
+    int minute = default;
+
+    [SerializeField]
+    int hour = default;
+
+    [SerializeField]
+    int day = default;
+    
+    [SerializeField]
+    int month = default;
+
+    [SerializeField]
+    int year = default;
+
+
+    public void OnBeforeSerialize()
+    {
+        millisecond = DateTime.Millisecond;
+        second = DateTime.Second;
+        minute = DateTime.Minute;
+        hour = DateTime.Hour;
+        day = DateTime.Day;
+        month = DateTime.Month;
+        year = DateTime.Year;
+    }
+
+    public void OnAfterDeserialize()
+    {
+        DateTime = new DateTime(year, month, day, hour, minute, second, millisecond);
+    }
+}
